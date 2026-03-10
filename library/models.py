cf1f4f322db0772598d7fbdd8dada3dd0984dc62
elinekp <eline.kaupang@gmail.com>
2026-03-10 21:52:23 +0100

    Change author deletion behavior to SET_NULL.

--- library/models.py
+++ library/models.py
@@ -9,7 +9,13 @@ class Author(models.Model):
 
 class Work(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='works')
+    author = models.ForeignKey(
+        Author, 
+        on_delete=models.SET_NULL,  # Endret fra CASCADE
+        null=True,                  # Påkrevd for SET_NULL
+        blank=True,                 # Tillater at feltet er tomt i skjemaer
+        related_name='works'
+    )
     summary = models.TextField(blank=True)
     original_publication_year = models.IntegerField(null=True, blank=True)
 
