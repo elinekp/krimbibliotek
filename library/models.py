5c40269517e1494d2728ca648892cc5d5417e8ab
elinekp <eline.kaupang@gmail.com>
2026-03-10 21:44:53 +0100

    Define Author and Work models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    biography = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Work(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='works')
+    summary = models.TextField(blank=True)
+    original_publication_year = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
