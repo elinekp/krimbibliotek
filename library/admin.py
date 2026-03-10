4bb8e30ba3ee1161f8ebbad7c292ee3d1686412f
elinekp <eline.kaupang@gmail.com>
2026-03-10 22:26:31 +0100

    Expand Author model and add Series support. Closes #7

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Author, Genre, AppealFactor, Work, Item
+from .models import Author, Genre, AppealFactor, Work, Item, Series
 
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(AppealFactor)
+admin.site.register(Series)
 admin.site.register(Work)
 admin.site.register(Item)
--- library/models.py
+++ library/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
+    nationality = models.CharField(max_length=100, blank=True)
+    birth_year = models.IntegerField(null=True, blank=True)
     biography = models.TextField(blank=True)
+    image_url = models.URLField(blank=True, verbose_name="Image URL")
+
+    def __str__(self):
+        return self.name
+
+class Series(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -30,13 +40,9 @@ class AppealFactor(models.Model):
 
 class Work(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(
-        Author, 
-        on_delete=models.SET_NULL, 
-        null=True, 
-        blank=True, 
-        related_name='works'
-    )
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, blank=True, related_name='works')
+    series = models.ForeignKey(Series, on_delete=models.SET_NULL, null=True, blank=True, related_name='works')
+    series_number = models.IntegerField(null=True, blank=True)
     genres = models.ManyToManyField(Genre, blank=True, related_name='works')
     appeal_factors = models.ManyToManyField(AppealFactor, blank=True, related_name='works')
     summary = models.TextField(blank=True)
@@ -52,14 +58,7 @@ class Item(models.Model):
         ('ebook', 'E-book'),
         ('audiobook', 'Audiobook'),
     ]
-
-    work = models.ForeignKey(
-        Work, 
-        on_delete=models.SET_NULL, # Endret fra CASCADE
-        null=True,                 # Påkrevd for SET_NULL
-        blank=True, 
-        related_name='items'
-    )
+    work = models.ForeignKey(Work, on_delete=models.SET_NULL, null=True, blank=True, related_name='items')
     isbn = models.CharField(max_length=13, blank=True, verbose_name="ISBN")
     publisher = models.CharField(max_length=255, blank=True)
     publication_year = models.IntegerField(null=True, blank=True)
